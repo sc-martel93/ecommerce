0f3d8724b86568392169fa0c578065a004bfd48e
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2021-04-26 23:40:14 -0400

    Added cart functionality

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,14 +4,7 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Pants', description: 'Blue jeans', price: '$8', image: 'https://media.istockphoto.com/photos/skinny-tight-blue-jeans-on-white-background-picture-id173239968?s=612x612' },
-    { id: 2, name: 'Shoes', description: 'Driving Shoes', price: '$80', image: 'https://media.istockphoto.com/photos/concept-of-rich-and-poor-in-person-picture-id599141986' }
-]
-
-
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles()
 
     return (
@@ -20,7 +13,7 @@ const Products = () => {
             <Grid container justify="center" spacing={4}>
                 {products.map(product => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
